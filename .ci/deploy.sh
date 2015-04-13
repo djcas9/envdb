ad9c888e39b97f4afa7c0e685defb49ab434bd9b
Dustin Willis Webber <dustin.webber@gmail.com>
2015-04-13 18:56:38 -0400

    update deploy.sh - revisit automation for packagecloud later

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -17,57 +17,57 @@ DEB386="${NAME}_${OLD_VERSION}_386.deb"
 if [ "$DRONE_BRANCH" = "$BRANCH" ] && [ "$DRONE_PR" != "true" ]; then
   echo "MASTER BRANCH: Deploying..."
 
-  if [ "$YANK_OLD_VERSIONS" = true ]; then
+  # if [ "$YANK_OLD_VERSIONS" = true ]; then
 
-    package_cloud yank $PREFIX/ubuntu/lucid   $DEB
-    package_cloud yank $PREFIX/ubuntu/hardy   $DEB
-    package_cloud yank $PREFIX/ubuntu/utopic  $DEB
-    package_cloud yank $PREFIX/ubuntu/precise $DEB
-    package_cloud yank $PREFIX/ubuntu/trusty  $DEB
+    # package_cloud yank $PREFIX/ubuntu/lucid   $DEB
+    # package_cloud yank $PREFIX/ubuntu/hardy   $DEB
+    # package_cloud yank $PREFIX/ubuntu/utopic  $DEB
+    # package_cloud yank $PREFIX/ubuntu/precise $DEB
+    # package_cloud yank $PREFIX/ubuntu/trusty  $DEB
 
-    package_cloud yank $PREFIX/ubuntu/lucid   $DEB386
-    package_cloud yank $PREFIX/ubuntu/hardy   $DEB386
-    package_cloud yank $PREFIX/ubuntu/utopic  $DEB386
-    package_cloud yank $PREFIX/ubuntu/precise $DEB386
-    package_cloud yank $PREFIX/ubuntu/trusty  $DEB386
+    # package_cloud yank $PREFIX/ubuntu/lucid   $DEB386
+    # package_cloud yank $PREFIX/ubuntu/hardy   $DEB386
+    # package_cloud yank $PREFIX/ubuntu/utopic  $DEB386
+    # package_cloud yank $PREFIX/ubuntu/precise $DEB386
+    # package_cloud yank $PREFIX/ubuntu/trusty  $DEB386
 
-    package_cloud yank $PREFIX/debian/squeeze $DEB
-    package_cloud yank $PREFIX/debian/jessie  $DEB
-    package_cloud yank $PREFIX/debian/wheezy  $DEB
+    # package_cloud yank $PREFIX/debian/squeeze $DEB
+    # package_cloud yank $PREFIX/debian/jessie  $DEB
+    # package_cloud yank $PREFIX/debian/wheezy  $DEB
 
-    package_cloud yank $PREFIX/debian/squeeze $DEB386
-    package_cloud yank $PREFIX/debian/jessie  $DEB386
-    package_cloud yank $PREFIX/debian/wheezy  $DEB386
+    # package_cloud yank $PREFIX/debian/squeeze $DEB386
+    # package_cloud yank $PREFIX/debian/jessie  $DEB386
+    # package_cloud yank $PREFIX/debian/wheezy  $DEB386
 
-  fi
+  # fi
 
-  #
-  # Push New Packages
-  #
+  # #
+  # # Push New Packages
+  # #
 
-  # Ubuntu - Push - amd64
-  package_cloud push $PREFIX/ubuntu/lucid   $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/ubuntu/hardy   $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/ubuntu/utopic  $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/ubuntu/precise $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/ubuntu/trusty  $RELEASE_PATH/$NAME-amd64.deb
+  # # Ubuntu - Push - amd64
+  # package_cloud push $PREFIX/ubuntu/lucid   $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/ubuntu/hardy   $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/ubuntu/utopic  $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/ubuntu/precise $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/ubuntu/trusty  $RELEASE_PATH/$NAME-amd64.deb
 
-  # Ubuntu - Push - 386
-  package_cloud push $PREFIX/ubuntu/lucid   $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/ubuntu/hardy   $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/ubuntu/utopic  $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/ubuntu/precise $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/ubuntu/trusty  $RELEASE_PATH/$NAME-386.deb
+  # # Ubuntu - Push - 386
+  # package_cloud push $PREFIX/ubuntu/lucid   $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/ubuntu/hardy   $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/ubuntu/utopic  $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/ubuntu/precise $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/ubuntu/trusty  $RELEASE_PATH/$NAME-386.deb
 
-  # Debian - Push - amd64
-  package_cloud push $PREFIX/debian/squeeze $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/debian/jessie  $RELEASE_PATH/$NAME-amd64.deb
-  package_cloud push $PREFIX/debian/wheezy  $RELEASE_PATH/$NAME-amd64.deb
+  # # Debian - Push - amd64
+  # package_cloud push $PREFIX/debian/squeeze $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/debian/jessie  $RELEASE_PATH/$NAME-amd64.deb
+  # package_cloud push $PREFIX/debian/wheezy  $RELEASE_PATH/$NAME-amd64.deb
 
-  # Debian - Push - 386
-  package_cloud push $PREFIX/debian/squeeze $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/debian/jessie  $RELEASE_PATH/$NAME-386.deb
-  package_cloud push $PREFIX/debian/wheezy  $RELEASE_PATH/$NAME-386.deb
+  # # Debian - Push - 386
+  # package_cloud push $PREFIX/debian/squeeze $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/debian/jessie  $RELEASE_PATH/$NAME-386.deb
+  # package_cloud push $PREFIX/debian/wheezy  $RELEASE_PATH/$NAME-386.deb
 
 
   # Upload To Github
@@ -75,10 +75,11 @@ if [ "$DRONE_BRANCH" = "$BRANCH" ] && [ "$DRONE_PR" != "true" ]; then
   tar jxf /tmp/github-release.tar.bz2 -C /tmp/ && sudo mv /tmp/bin/linux/amd64/github-release /usr/local/bin/github-release
 
   result=$(github-release release -u mephux -r envdb -t v$(cat .Version) -n "v$(cat .Version)" -d ""   || true)
-  if [[ $result == *422* ]]; then
+
+  if [ $result == *422* ]; then
     echo -e "Release already exists for this tag.";
     exit 0
-  elif [[ $result == "" ]]; then
+  elif [ $result == "" ]; then
     echo -e "Release created.";
   else
     echo -e "Error creating release: $result"
